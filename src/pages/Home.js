4b3a64daef27e1f9a911dfd2d964195f2ba91c28
Alabi johnson <abiodunalabijohn@gmail.com>
2019-07-04 14:09:27 +0100

    'navbaradded'

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar  from '../components/Navbar';
+
+import '../App.css';
+
+
+function Home() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default Home;
